717d1b6700497e70af956e0e35fdf670008b17c0
Dmitry Pelevin <dpelevin@navicons.ru>
2017-02-08 19:40:20 +0300

    #1 Init Service Provider

---
+++ ServiceProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KpdApps.Crm.Ms.Common
+{
+	public sealed class ServiceProvider : IServiceProvider
+	{
+		public object GetService(Type serviceType)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
